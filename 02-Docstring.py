b3b77f80000d3c9ac73ec47694d42a3e4c5c4156
jmorabaiz <jmorabaiz@gmail.com>
2023-09-14 00:29:13 -0300

    Se finaliza con Doctest

--- 02-Docstring.py
+++ 02-Docstring.py
@@ -1,5 +1,23 @@
+"""Este es el Docstring del módulo 02-Docstring."""
+
 # Docstring
 # __doc__
+
+
+class User:
+    """Permite representar un usuario"""
+
+    def __init__(self, username: str, password: str) -> None:
+        """
+        Permite instanciar un objeto de tipo User.
+
+        :param username: str
+        :param password: str
+        """
+
+        self.username = username
+        self.password = password
+
 
 def palindromo(sentence: str) -> bool:
     """
@@ -9,21 +27,27 @@ def palindromo(sentence: str) -> bool:
     :return: bool
     :examples:
 
-    >>> palindromo('Anita lava la tina')
-    True
+    # >>> palindromo('Anita lava la tina')
+    # True
+    #
+    # >>> palindromo('CodigoFacilito')
+    # False
+    #
+    # >>> sentence = 'Oso'
+    # >>> palindromo(sentence)
+    # True
+    # """
 
-    >>> palindromo('CodigoFacilito')
-    False
+    # Se pasa a un archivo plano, ya que es lo correcto para correr estos test
+    # python -m doctest test_02-Docstring.txt -v
 
-    >>> sentence = 'Oso'
-    >>> palindromo(sentence)
-    True
-    """
     sentence = sentence.lower().replace(' ', '')
     return sentence == sentence[::-1]
 
 # In python shell you can use:
 # 1. docstring = __import__("02-Docstring")
 # 2. docstring.palindromo.__doc__ or help(docstring.palindromo)
+# 3. docstring.__doc__ or docstring.User.__doc__ or docstring.User.__init__.__doc__
+# 4. help(docstring.User.__init__) - Best option
 
 # To run doctest in terminal run "python -m doctest 02-Docstring.py -v"
